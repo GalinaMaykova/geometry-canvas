2aaa740c463069ea18ab302b0e38855583121241
g.maikova <g.maikova@prosoftsystems.ru>
2026-07-09 19:34:48 +0500

    fix

--- src/scripts/canvas.js
+++ src/scripts/canvas.js
@@ -1,14 +1,1 @@
-export const canvas = document.getElementById('canvas');
-export const ctx = canvas.getContext('2d');
-export const W = canvas.width;
-export const H = canvas.height;
-
-export function getMousePos(e) {
-    const rect = canvas.getBoundingClientRect();
-    const scaleX = canvas.width / rect.width;
-    const scaleY = canvas.height / rect.height;
-    return {
-        x: (e.clientX - rect.left) * scaleX,
-        y: (e.clientY - rect.top) * scaleY
-    };
-}
+...
